1f743f08bd8d63c9bd12acad0e90fe8fe9cd701e
Cocci <87042895+C0cci@users.noreply.github.com>
2021-07-06 23:53:23 +0200

    index
    node index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-fqsd
+const Discord = require("discord.js");
+const Client = new Discord.Client;
+
+const prefix = "!";
+
+Client.on("message", message => {
+    message.delete()
+    if(message.author.bot) if(message.channel.type === "dm") return;
+    if(message.content === prefix + "ping"){
+      message.channel.send(":ping_pong: Pong!")
+    }
+})
+
+Client.login("VOTRE TOKEN");
